d39b3fdc1e7ac0f99c5d9f0cc30361a13687ef72
Александр <sh889@ya.ru>
2021-12-16 19:50:49 +0500

    Верстка и оживление слайдера

--- source/js/compare.js
+++ source/js/compare.js
@@ -1,19 +1,62 @@
+const WITH_TABLET = 768;
+
 const compare = document.querySelector('.compare');
 const compareItemBefore = compare.querySelector('.compare__item--before');
 const compareItemAfter = compare.querySelector('.compare__item--after');
-const beforeBtn = compare.querySelector('.compare__toggle--before');
-const afterBtn = compare.querySelector('.compare__toggle--after');
+const controls = compare.querySelector('.compare__controls');
+const beforeBtn = controls.querySelector('.compare__toggle--before');
+const afterBtn = controls.querySelector('.compare__toggle--after');
+const scaleToggle = controls.querySelector('.compare__scale-toggle');
+
+window.addEventListener(`resize`, main);
+scaleToggle.addEventListener('input', draw);
 
 beforeBtn.addEventListener('click', () => {
-  compareItemBefore.classList.add('compare__item--current');
-  compareItemAfter.classList.remove('compare__item--current');
-  beforeBtn.classList.add('compare__toggle--current');
-  afterBtn.classList.remove('compare__toggle--current');
+  scaleToggle.value = document.body.clientWidth < WITH_TABLET ? 1 : 100;
+  draw();
 });
 
 afterBtn.addEventListener('click', () => {
-  compareItemBefore.classList.remove('compare__item--current');
-  compareItemAfter.classList.add('compare__item--current');
-  beforeBtn.classList.remove('compare__toggle--current');
-  afterBtn.classList.add('compare__toggle--current');
+  scaleToggle.value = document.body.clientWidth < WITH_TABLET ? 2 : 1;
+  draw();
 });
+
+
+function draw() {
+  value = scaleToggle.value;
+
+  if (document.body.clientWidth < WITH_TABLET) {
+    if (value == 1) {
+      compareItemBefore.classList.add('compare__item--current');
+      compareItemAfter.classList.remove('compare__item--current');
+    } else {
+      compareItemBefore.classList.remove('compare__item--current');
+      compareItemAfter.classList.add('compare__item--current');
+    }
+
+    compareItemBefore.style.width = '';
+    compareItemAfter.style.width = '';
+    compareItemAfter.style.marginLeft = '';
+  } else {
+    compareItemBefore.style.width = `${value}%`;
+
+    compareItemAfter.style.width = `${100 - value}%`;
+    compareItemAfter.style.marginLeft = `${value}%`;
+  }
+}
+
+function main() {
+  compare.classList.remove('compare--nojs');
+
+  if (document.body.clientWidth < WITH_TABLET) {
+    scaleToggle.value = 1;
+    scaleToggle.max = 2;
+  } else {
+    scaleToggle.value = 50;
+    scaleToggle.max = 100;
+  }
+
+  draw();
+}
+
+main();
